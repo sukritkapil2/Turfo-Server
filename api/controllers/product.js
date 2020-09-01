c90fe1dc89b0dbff0b3929411a4b0d28c0e525ce
sukritkapil2 <sukrit.kapil2@gmail.com>
2020-09-02 01:15:53 +0530

    Product Details Added

--- api/controllers/product.js
+++ api/controllers/product.js
@@ -2,6 +2,60 @@ const mongoose = require("mongoose");
 
 const Category = require('../models/Category');
 const Product = require('../models/Product');
+const Shop = require('../models/Shop')
+
+exports.get_specific_product = (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+        .exec()
+        .then(product => {
+            if (product) {
+                res.status(200).json(product)
+            } else {
+                res.status(404)
+                    .json({
+                        message: "No document found with this id"
+                    })
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({
+                err: err
+            })
+        })
+}
+
+exports.get_products_shop = (req, res, next) => {
+
+    const id = req.params.shopId;
+
+    if (id == null || id == undefined) {
+        return res.status(400).json({
+            message: "Please Send Shop ID"
+        })
+    }
+
+    Shop.findById(id)
+        .exec()
+        .then((result) => {
+            var productsArray = result.products;
+
+            Product.find({
+                _id: {
+                    $in: productsArray
+                }
+            })
+                .exec()
+                .then((resultProducts) => {
+                    res.status(200).json(resultProducts)
+                })
+                .catch((err) => {
+                    res.status(500).json({
+                        err: err
+                    })
+                })
+        })
+}
 
 exports.get_trending = (req, res, next) => {
     const city = req.query.city;
--- api/routes/product.js
+++ api/routes/product.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const ProductController = require('../controllers/product')
 
+
 router.get("/categories", ProductController.get_categories);
 router.get("/categories/:category", ProductController.get_product_category);
 router.get("/trending", ProductController.get_trending)
+router.get("/shop/:shopId", ProductController.get_products_shop);
+router.get("/:productId", ProductController.get_specific_product);
 
 module.exports = router;
